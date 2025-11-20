6a914bede774ae690cec511d693f25c75d21c241
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-20 14:31:07 +0700

    Update util.py

--- app/menus/util.py
+++ app/menus/util.py
@@ -22,9 +22,9 @@ def print_banner():
         banner_text,
         border_style=theme["border_primary"],
         style=theme["text_title"],
-        padding=(0, 2),
+        padding=(1, 2),
         expand=True,
-        #box=box.DOUBLE
+        box=box.DOUBLE
     ))
 
 def clear_screen():
