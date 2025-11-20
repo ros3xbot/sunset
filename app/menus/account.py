bf12495fc2db0ac96c030907c25e34bede9a00a1
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-20 16:42:43 +0700

    Update account.py

--- app/menus/account.py
+++ app/menus/account.py
@@ -5,7 +5,7 @@ from rich.align import Align
 from rich.box import MINIMAL_DOUBLE_HEAD
 
 from app.client.ciam import get_otp, submit_otp
-from app.menus.util import clear_screen, pause, print_panel, nav_range
+from app.menus.util import clear_screen, clear_sc, pause, print_panel, nav_range
 from app.service.auth import AuthInstance
 from app.config.theme_config import get_theme
 from app.service.service import load_status, save_status
@@ -23,7 +23,7 @@ def normalize_number(raw_input: str) -> str:
     return raw_input
 
 def login_prompt(api_key: str):
-    clear_screen()
+    clear_sc()
     theme = get_theme()
     console.print(Panel(
         Align.center("🔐 Login ke MyXL", vertical="middle"),
@@ -72,7 +72,7 @@ def login_prompt(api_key: str):
         return None, None
 
 def show_account_menu():
-    clear_screen()
+    clear_sc()
     theme = get_theme()
     AuthInstance.load_tokens()
     users = AuthInstance.refresh_tokens
