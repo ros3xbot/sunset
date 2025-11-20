b73185af69beeec8384ce714fcd679c559475e4c
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-20 14:46:06 +0700

    Update util.py

--- app/menus/util.py
+++ app/menus/util.py
@@ -8,11 +8,12 @@ from rich.table import Table
 from rich.align import Align
 from rich import box
 from app.config.theme_config import get_theme, get_theme_style
-from app.service.auth import AuthInstance
 
 console = Console()
 
+
 def simple_number():
+    from app.service.auth import AuthInstance
     theme = get_theme()
     active_user = AuthInstance.get_active_user()
 
