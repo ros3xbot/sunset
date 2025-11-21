43446318a25f4caadf4bbda844c5afe3c6787a5d
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-21 15:03:15 +0700

    Update util.py

--- app/menus/util.py
+++ app/menus/util.py
@@ -77,7 +77,7 @@ def clear_sc():
 |______  /(____  /__|  |___  /\___  >__/\_ \
        \/      \/          \/     \/      \/"""
 
-    version_text = f"[{get_theme_style('text_warn')}]myXL CLI v8.9.1 sunset[/{get_theme_style('text_warn')}]"
+    version_text = f"[{get_theme_style('text_sub')}]myXL CLI v8.9.1 sunset[/{get_theme_style('text_sub')}]"
     
     content = f"{ascii_art}\n           {version_text}"
     console.print(
