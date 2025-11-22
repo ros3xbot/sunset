35d5c4cba752b74b73f4515214ead47c90d9f732
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-22 09:47:13 +0700

    sunset

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 from rich.panel import Panel
 from rich.text import Text
 from rich.console import Console
-from app.service.git import ensure_git
+from app.service.git import check_for_updates, ensure_git
 from app.menus.util import pause, live_loading, print_panel, print_error
 from app.config.theme_config import get_theme, get_theme_style
 
@@ -153,6 +153,10 @@ def run_main():
         sys.exit(1)
 
 if __name__ == "__main__":
-    ensure_git()
-    git_pull_rebase()
-    run_main()
+    try:
+        with live_loading("🔄 Checking for updates...", get_theme()):
+            need_update = check_for_updates()
+        ensure_git()
+        git_pull_rebase()
+        run_main()
+
