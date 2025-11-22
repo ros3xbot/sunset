748a422dff81352cc19cdcc342c4cc6095271fb0
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-22 08:08:12 +0700

    sunset

--- app/client/engsel.py
+++ app/client/engsel.py
@@ -174,7 +174,7 @@ def get_package(api_key: str, tokens: dict,
     with live_loading(f"📦 Fetching package...", get_theme()):
         res = send_api_request(api_key, path, raw_payload, tokens["id_token"], "POST")
     if "data" not in res:
-        print_error("❌", f"Gagal mengambil package: {package_family_code}")
+        print_error("❌", f"Gagal mengambil package: {package_option_code}")
         print_panel("📑 Response", json.dumps(res, indent=2))
         return None
     return res["data"]
