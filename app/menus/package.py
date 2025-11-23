86e1ebd6d05a83b2c956c01f6eb4624b0693c819
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-23 18:38:37 +0700

    sunset

--- app/menus/package.py
+++ app/menus/package.py
@@ -686,7 +686,7 @@ def fetch_my_packages():
 
                     benefit_table.add_row(name, dt, f"{r_str} / {t_str}")
 
-            with live_loading(f"🔍 {num}...", theme):
+            with live_loading(f"🔍 {num}", theme):
                 package_details = get_package(api_key, tokens, quota_code)
             if package_details:
                 family_code = package_details["package_family"]["package_family_code"]
