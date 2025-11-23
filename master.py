19b13b55ea7b164c7ccbfbddff85550ae3d4f5ab
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-23 10:32:24 +0700

    sunset

--- master.py
+++ master.py
@@ -137,7 +137,7 @@ def show_main_menu(profile: dict, display_quota: str, segments: dict):
     menu_table.add_row("77", f"[{theme['text_sub']}]🎨 Ganti Tema CLI[/]")
     menu_table.add_row("88", f"[{theme['border_warning']}]☕ Menu Berikutnya[/]")
     menu_table.add_row("99", f"[{theme['text_err']}]⛔ Tutup Aplikasi[/]")
-    menu_table.add_row("C", f"[{theme['text_title']}]🧹 Bersihkan Cache akun[/]")
+    menu_table.add_row("C", f"[{theme['text_title']}]🧹 Bersihkan Cache[/]")
 
     console.print(
         Panel(
