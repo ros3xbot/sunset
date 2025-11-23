54a1280062089ffb4808e523399ccf2250728287
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-24 04:58:56 +0700

    sunset

--- app/menus/info.py
+++ app/menus/info.py
@@ -63,7 +63,7 @@ def show_info_menu():
     console.print(Panel(
         Align.center(qr_code_ascii),
         title=f"[{theme['text_title']}]📱 Scan QRIS[/]",
-        border_style=theme["border_succes"],
+        border_style=theme["border_success"],
         padding=(1, 2),
         expand=True,
         title_align="center"
