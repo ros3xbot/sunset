1bcc12a4a8cb7072a93ad03a3786ec805c7c611a
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-24 05:00:37 +0700

    sunset

--- app/menus/info.py
+++ app/menus/info.py
@@ -18,6 +18,7 @@ def generate_qr_ascii(data: str) -> str:
     qr.print_ascii(out=output, invert=True)
     return output.getvalue()
 
+
 def show_info_menu():
     clear_screen()
     theme = get_theme()
