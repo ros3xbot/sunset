d4583e7230a4cb794d29a0a2b4b5af163ead30a8
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-24 12:14:31 +0700

    sunset

--- app/menus/util.py
+++ app/menus/util.py
@@ -42,7 +42,7 @@ def simple_number():
 
     console.print(Panel(
         Align.center(text),
-        border_style=get_theme_style("text_primary"),
+        border_style=get_theme("text_primary"),
         padding=(0, 0),
         expand=True
     ))
