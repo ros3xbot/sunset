9658bbe2fbf79d6991065de399bf66a0f49ab9ec
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-24 14:02:35 +0700

    sunset

---
+++ app/config/imports.py
@@ -0,0 +1,54 @@
+# Rich
+from rich.console import Console
+from rich.panel import Panel
+from rich.table import Table
+from rich.text import Text
+from rich.align import Align
+from rich.box import MINIMAL_DOUBLE_HEAD
+
+# Utilitas internal
+from app.menus.util import (
+    clear_screen,
+    pause,
+    print_panel,
+    get_rupiah,
+    display_html,
+    simple_number,
+    format_quota_byte,
+    nav_range
+)
+
+# Service / type yang sering dipakai
+from app.service.auth import AuthInstance
+from app.service.decoy import DecoyInstance
+from app.service.bookmark import BookmarkInstance
+from app.client.engsel import get_family, get_package, get_package_details
+from app.type_dict import PaymentItem
+
+# Inisialisasi console global
+console = Console()
+
+# __all__ untuk kontrol import *
+__all__ = [
+    "console",
+    "Panel",
+    "Table",
+    "Text",
+    "Align",
+    "MINIMAL_DOUBLE_HEAD",
+    "clear_screen",
+    "pause",
+    "print_panel",
+    "get_rupiah",
+    "display_html",
+    "simple_number",
+    "format_quota_byte",
+    "nav_range",
+    "AuthInstance",
+    "DecoyInstance",
+    "BookmarkInstance",
+    "get_family",
+    "get_package",
+    "get_package_details",
+    "PaymentItem",
+]
