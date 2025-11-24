8fb316183238549c667782bf6481991f7ba1c99b
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-24 17:27:14 +0700

    sunset

--- app/menus/package.py
+++ app/menus/package.py
@@ -85,7 +85,7 @@ def show_package_details(api_key, tokens, package_option_code, is_enterprise, op
 
     # Panel Info Paket
     info_table = Table.grid(padding=(0, 1))
-    info_table.add_column(justify="left", style=theme["text_body"])
+    info_table.add_column(justify="left", style=theme["border_info"])
     info_table.add_column(justify="left")
     info_table.add_row("Nama", f": [bold {theme['text_body']}]{title}[/]")
     info_table.add_row("Harga", f": Rp [{theme['text_money']}]{formatted_price}[/]")
