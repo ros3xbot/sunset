5199c13463bae43dbe659288f67acdc3a6e3690b
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-25 12:19:11 +0700

    sunset

--- app/client/store/segments.py
+++ app/client/store/segments.py
@@ -1,6 +1,6 @@
 import json
 from app.client.engsel import send_api_request
-from app.menus.util import live_loading, print_error, print_success, print_panel
+from app.menus.util import live_loading
 from app.config.theme_config import get_theme
 
 
@@ -12,9 +12,6 @@ def get_segments(api_key: str, tokens: dict, is_enterprise: bool = False) -> dic
         res = send_api_request(api_key, path, payload, tokens["id_token"], "POST")
 
     if not res or res.get("status") != "SUCCESS":
-        print_error("❌", "Failed to fetch segments.")
-        print_panel("📑 Response", json.dumps(res, indent=2))
         return None
 
-    print_success("✅", "Store segments fetched successfully")
     return res
