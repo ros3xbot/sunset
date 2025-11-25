f8c85166ab89d33c72885a1cdb1cdeec251db63d
ros3xbot <161305253+ros3xbot@users.noreply.github.com>
2025-11-25 07:13:46 +0700

    sunset

--- app/menus/bookmark.py
+++ app/menus/bookmark.py
@@ -1,17 +1,5 @@
 from app.config.imports import *
-
 from app.menus.util import clear_screen, simple_number, pause, print_panel
-#from app.config.theme_config import get_theme
-#from app.service.auth import AuthInstance
-#from app.service.bookmark import BookmarkInstance
-#from app.client.engsel import get_family, get_package_details
-#from app.menus.package import show_package_details
-#from app.service.git import ensure_git
-#from rich.console import Console
-#from rich.panel import Panel
-#from rich.align import Align
-#from rich.table import Table
-#from rich.box import MINIMAL_DOUBLE_HEAD
 
 console = Console()
 
